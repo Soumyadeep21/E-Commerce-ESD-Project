c274996bf5dab48039d1e35c22ed1ced17db3ee7
Soumyadeep <soumo.sinha21@gmail.com>
2019-07-01 01:05:23 +0530

    Update items.dart

--- lib/items.dart
+++ lib/items.dart
@@ -1,9 +1,9 @@
-class Item{
+class Item {
   final int id;
   final String name;
   final double price;
-  final double description;
+  final String description;
+  final String img;
 
-  Item({this.id, this.name, this.price, this.description});
-  
+  Item({this.img, this.id, this.name, this.price, this.description});
 }
