0d555d57d3dc99d5699ca0fbc9d7de13979e14db
Soumyadeep <soumo.sinha21@gmail.com>
2019-07-01 00:02:12 +0530

    Update suggestions.dart

--- lib/suggestions.dart
+++ lib/suggestions.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class Suggestions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Text(
+        'You have no suggestions to show currently! Click the chat button to ask for suggestions from our executive . ',
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
