5e4e78f89b93835762ed495ca19964df58a2051c
Soumyadeep <soumo.sinha21@gmail.com>
2019-07-01 00:02:00 +0530

    Update categories.dart

--- lib/categories.dart
+++ lib/categories.dart
@@ -3,6 +3,76 @@ import 'package:flutter/material.dart';
 class Categories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView(
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      children: <Widget>[
+        Card(
+          elevation: 10.0,
+          margin: EdgeInsets.all(10.0),
+          child: Center(
+            child: Text(
+              'Laptops',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Card(
+          elevation: 10.0,
+          margin: EdgeInsets.all(10.0),
+          child: Center(
+            child: Text(
+              'Mobile Phones',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Card(
+          elevation: 10.0,
+          margin: EdgeInsets.all(10.0),
+          child: Center(
+            child: Text(
+              'Power Banks',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Card(
+          elevation: 10.0,
+          margin: EdgeInsets.all(10.0),
+          child: Center(
+            child: Text(
+              'Headsets / Headphones',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Card(
+          elevation: 10.0,
+          margin: EdgeInsets.all(10.0),
+          child: Center(
+            child: Text(
+              'Computer Accessories',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Card(
+          elevation: 10.0,
+          margin: EdgeInsets.all(10.0),
+          child: Center(
+            child: Text(
+              'Fitness Bands',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
