30befd3a0c08d43253dc5f4afb400d2ff86e2e29
Soumyadeep <soumo.sinha21@gmail.com>
2019-07-01 00:02:05 +0530

    Update local_stores.dart

--- lib/local_stores.dart
+++ lib/local_stores.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class LocalStores extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Text('Stores near you will be shown in this area !'),
+    );
   }
 }
