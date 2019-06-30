c7ba3ab0a7a4cd00e775a7068d1110be565e3573
Soumyadeep <soumo.sinha21@gmail.com>
2019-07-01 01:05:35 +0530

    Create products.dart

---
+++ lib/products.dart
@@ -0,0 +1,52 @@
+import 'items.dart';
+
+List<Item> laptops = [
+  Item(
+    name:
+        "Lenovo Ideapad 330 Ryzen 5 Quad Core - (8 GB/1 TB HDD/DOS) 330-15ARR Laptop  (15.6 inch, Platinum Grey, 2.2 kg)",
+    description:
+        "Indulge in the world of entertainment in a much more immersive way by bringing home this laptop from Lenovo. Featuring a 39.62-centimetre HD anti-glare display and Dolby Audio, this laptop offers picture- and audio-quality that'll take your entertainment experience to a whole new level. And, if it runs out of charge, leaving you in the middle of your favourite movie, then you can get it up and running in no time as it also supports Rapid Charge.",
+    img: 'assets/laptop1.jpeg',
+    price: 39999,
+  ),
+  Item(
+    name:
+        "Acer Aspire 5 Core i5 7th Gen - (8 GB/1 TB HDD/Windows 10 Home/2 GB Graphics) A515-51G -5673 Laptop  (15.6 inch, Obsidian Black, 2 kg)",
+    description:
+        "Pre-installed Genuine Windows 10 OS NVIDIA Geforce MX130 for High Graphics Performance Light Laptop without Optical Disk Drive 15.6 inch Full HD LED Backlit Anti-glare TFT Display",
+    img: 'assets/laptop2.jpeg',
+    price: 49212,
+  ),
+  Item(
+    name:
+        "Asus Core i5 8th Gen - (8 GB/1 TB HDD/Windows 10 Home) X540UA-DM995T Laptop  (15.6 inch, Black, 2 kg)",
+    description:
+        "Stand apart from the crowd with the lightweight ASUS X540UA. It comes with an Intel Core i5 8th Generation Processor, 8 GB RAM, 39.62 cm HD LED Backlit display that protects you from eye problems with the help of the ASUS Eye Care technology. Enjoy watching your favourite entertainment after choosing one of the five audio modes offered by the ASUS SonicMaster Audio. The elegant and premium brushed finish of this laptop is bound to make head turns wherever you go.",
+    img: 'assets/laptop3.jpeg',
+    price: 43990,
+  ),
+  Item(
+    name:
+        "Acer Nitro 5 Ryzen 5 Quad Core - (8 GB/1 TB HDD/Windows 10 Home/4 GB Graphics) AN515-42-R6GV Gaming Laptop  (15.6 inch, Black, 2.7 kg)",
+    description:
+        "15.6 inch Full HD LED Backlit IPS Display Light Laptop without Optical Disk Drive Pre-installed Genuine Windows 10 OS",
+    img: 'assets/laptop5.jpeg',
+    price: 49990,
+  ),
+  Item(
+    name:
+        "HP Pavilion x360 Core i3 8th Gen - (4 GB/256 GB SSD/Windows 10 Home) 14-dh0107TU 2 in 1 Laptop  (14 inch, Natural Silver, 1.59 kg, With MS Office)",
+    description:
+        "Carry It Along 2 in 1 Laptop 14 inch HD LED Backlit Anti-glare Touch Display (Support Active Stylus Pen) Light Laptop without Optical Disk Drive",
+    img: 'assets/laptop4.jpeg',
+    price: 42990,
+  ),
+  Item(
+    name:
+        "Asus ROG Strix Core i5 8th Gen - (8 GB/1 TB HDD/256 GB SSD/Windows 10 Home/4 GB Graphics) GL503GE-EN269T Gaming Laptop  (15.6 inch, Traditional Black, 2.6 kg)",
+    description:
+        "Boasting powerful and innovative features, this Asus laptop is truly a gamer's delight. It comes with an Intel Core i5 processor (8th Gen) and 8 GB of RAM to offer you a lag-free multitasking experience. It also features 4 GB of dedicated graphics memory for an enhanced gaming and video-watching experience.",
+    img: 'assets/laptop6.jpeg',
+    price: 84990,
+  )
+];
