522d75344e648982dabd7b490b655fd67ea236d7
Soumyadeep <soumo.sinha21@gmail.com>
2019-07-01 11:35:33 +0530

    Update products.dart

--- lib/products.dart
+++ lib/products.dart
@@ -48,5 +48,117 @@ List<Item> laptops = [
         "Boasting powerful and innovative features, this Asus laptop is truly a gamer's delight. It comes with an Intel Core i5 processor (8th Gen) and 8 GB of RAM to offer you a lag-free multitasking experience. It also features 4 GB of dedicated graphics memory for an enhanced gaming and video-watching experience.",
     img: 'assets/laptop6.jpeg',
     price: 84990,
-  )
+  ),
+  Item(
+    name:
+        "Dell Vostro 15 3000 Core i5 8th Gen - (8 GB/1 TB HDD/DOS/2 GB Graphics) VOS 3578 Laptop  (15.6 inch, Black, 2.18 kg)",
+    description:
+        "You can get things done easily with this laptop that is powered by the 8th Gen Intel Core i5 Processor. You can also choose the built-in discrete graphics from AMD, with a GDDR5 RAM of 2 GB for an added boost. Oh, and if you have the habit of opening multiple applications, you can easily switch between them without any lag thanks to a DDR4 Memory of 8 GB and an HDD Capacity of 1 TB.",
+    img: 'assets/laptop7.jpeg',
+    price: 50000,
+  ),
+];
+
+List<Item> mobiles = [
+  Item(
+      name: 'Asus ZenFone Max Pro M2 (Titanium, 64 GB)  (6 GB RAM)',
+      description:
+          "If you're looking for a smartphone that's robust and powerful, both, inside and outside, then your search ends here. Meet the ZenFone Max Pro M2 - the smartphone that comes loaded with innovative features, such as a Snapdragon 660 processor, a 5-magnet NXP speaker and Corning Gorilla Glass 6 protection, to make your mobile experience all the more seamless and secure.",
+      img: 'assets/mobile1.jpeg',
+      price: 13999),
+  Item(
+      name: 'Google Pixel 3 (Just Black, 128 GB)  (4 GB RAM)',
+      description:
+          "Staying too far from your loved ones? Video call them for hours on end. The weather is romantic? Listen to your favourite playlists all day long. Don't want to go out this weekend? Then binge watch your favourite series on the Internet or perhaps play your favourite mobile video game; do all that and much more. The Pixel 3 ensures that there's never a dull moment, all thanks to its powerful battery, impressive cameras and its expansive bezel-less display",
+      img: 'assets/mobile2.jpeg',
+      price: 58999),
+  Item(
+      name: 'Redmi Note 7 Pro (Neptune Blue, 64 GB)  (4 GB RAM)',
+      description:
+          "It's time to go big with the Redmi Note 7 Pro's 16-cm (6.3) FHD+ Dot Notch display. Powered by a 2.0 GHz Qualcomm Snapdragon 675 processor and 4 GB of RAM, this smartphone lets you experience the next level of performance and control. With a (48 MP + 5 MP) dual-rear camera, a 13 MP front camera, and features such as Face Unlock and 4K Video Recording, the Redmi Note 7 Pro truly puts a new spin on your smartphone experience.",
+      img: 'assets/mobile3.jpeg',
+      price: 13999),
+  Item(
+      name: 'Realme 3 (Radiant Blue, 32 GB)  (3 GB RAM)',
+      description:
+          "Stylish to flaunt, perfect to click, ideal to play your favourite games with - the Realme 3 smartphone has everything in place to help you do your tasks easily. It is powered by the Helio P70 AI processor, which is ideal for gaming purposes. That's not all, with the presence of a 13 MP + 2 MP dual rear camera and a 13 MP front camera, you can click beautiful pictures of everything that's around you.",
+      img: 'assets/mobile4.jpeg',
+      price: 8999),
+  Item(
+      name: 'Samsung Galaxy A50 (White, 64 GB)  (4 GB RAM)',
+      description:
+          "Do a lot more than just text and call your friends with the Samsung Galaxy A50 smartphone. The Exynos 9610 Octa-core Processor makes multitasking a breeze. Take photography a notch higher with its revolutionary Triple Camera System that comprises a 25 MP Low Light Camera, 8 MP Ultra-wide Camera and 5 MP Live Focus Camera.",
+      img: 'assets/mobile5.jpeg',
+      price: 18490),
+  Item(
+      name: 'POCO F1 by Xiaomi (Graphite Black, 64 GB)  (6 GB RAM)',
+      description:
+          "Meet the POCO F1 - the first flagship smartphone from POCO by Xiaomi. The POCO F1 sports Qualcomm flagship Snapdragon 845 processor, an octa-core CPU with a maximum clock speed of 2.8 GHz which is supported by 6 GB of LPDDR4X RAM. It is coupled with a LiquidCool Technology that allows the device to sustain peak performance for a longer period of time. On the back, it features a 12MP + 5MP Dual Pixel AI dual camera setup. The main camera sensor features 1.4 um pixels, Dual Pixel Autofocus, and Multi-frame noise reduction. On the front, it sports a 20 MP high-res front camera and IR Face unlock. POCO F1 also boasts of a massive 4000 mAh (typ) battery with Quick Charge 3.0 to keep you going all-day long.",
+      img: 'assets/mobile6.jpeg',
+      price: 17999),
+  Item(
+      name: 'Apple iPhone X (Space Gray, 64 GB)',
+      description:
+          "Meet the iPhone X - the device that's so smart that it responds to a tap, your voice, and even a glance. Elegantly designed with a large 14.73 cm (5.8) Super Retina screen and a durable front-and-back glass, this smartphone is designed to impress. What's more, you can charge this iPhone wirelessly.",
+      img: 'assets/mobile7.jpeg',
+      price: 91900),
+  Item(
+      name: 'OPPO F11 (Marble Green, 128 GB)  (4 GB RAM)',
+      description:
+          "Gone are the days when you had to carry your camera to get good quality pictures. With the Oppo F11, you can click professional quality pictures that are crisp, clear, and vibrant. It comes with a plethora of features that enhance the quality of your photos and lends them a professional touch. Its AI powered Ultra-care engine also lets you get high-quality pictures at night without any blurring or distortion. Besides these features, it is also equipped with a powerful battery that lets you use the phone without running out of power frequently. ",
+      img: 'assets/mobile8.jpeg',
+      price: 17990),
+];
+
+List<Item> headphones = [
+  Item(
+    name: 'Mi Basic Wired Headset with Mic  (Red, In the Ear)',
+    description:
+        "With Mic:Yes Connector type: 3.5mm headphone jack Extra bass: Add extra thump to your music One button universal remote to answer and manage your calls",
+    img: 'assets/headset1.jpeg',
+    price: 399,
+  ),
+  Item(
+    name: 'Samsung Level U Bluetooth Headset with Mic  (Black, In the Ear)',
+    description:
+        "The Samsung Level U Wireless Headset offers a Comfortable Fit and More Battery Power so that your day is filled with your favourite music. It also comes with Microphones that you can use to enjoy crystal-clear voice quality.",
+    img: 'assets/headset2.jpeg',
+    price: 2499,
+  ),
+  Item(
+    name: 'JBL T160 Wired Headset with Mic  (Blue, In the Ear)',
+    description:
+        "Introducing JBL T160 in-ear headphones. They are lightweight, comfortable and compact. Under the durable earpiece housing, a pair of 9mm drivers punch out some serious bass, reproducing the JBL Pure Bass sound you have experienced in concert halls, arenas and recording studios around the world. In addition, the single-button remote on a tangle-free flat cable lets you control music playback, as well as answer calls on the fly with a built-in microphone. Making the JBL T160 your everyday companion for work, at home and on the road.",
+    img: 'assets/headset3.jpeg',
+    price: 799,
+  ),
+  Item(
+    name:
+        'boAt BassHeads 220 Super Extra Bass Wired Headset with Mic  (Blue, In the Ear',
+    description:
+        "boAt BassHeads 220 Polished Metal Earphones are designed to give you an Experience which no other brand can match. It has an incredible sonic clarity with Super Extra Bass. It is not a plastic earphone - It is Polished Metal. With tangle free flat cables, you are expected to be hassle free. As the headphones feature 10mm drivers, they give you clear sound with that Thumping Bass. In Built Noise Cancelling Mic ensures you don’t miss those important calls. With Colours that match your style in your day, the boAt earphones are the ideal choice for anyone who can carry that attitude. Just Plug Them in and Plug into Nirvana!",
+    img: 'assets/headset4.jpeg',
+    price: 549,
+  ),
+  Item(
+    name:
+        'JBL T450BT Extra Bass Bluetooth Headset with Mic  (Black, On the Ear)',
+    description:
+        "Shake a leg, headbang, or groove to your favorite songs without disturbing others with the JBL T450 BT Bluetooth Headset. This mobile accessory features a Flat and Foldable Design, Bluetooth Connectivity, and JBL Pure Bass Sound for an immersive aural experience wherever you go.",
+    img: 'assets/headset5.jpeg',
+    price: 2199,
+  ),
+  Item(
+    name: 'Sennheiser HD 206 Wired Headphone  (Black, Over the Ear)',
+    description:
+        "General Model Name HD 206 Color Black Headphone Type Over the Ear Inline Remote No Sales Package 1 Headphone Connectivity Wired Product Details Deep Bass No Circumaural/Supraaural Circumaural Other Features High-quality Leatherette Ear Pads, Sound Pressure Level (SPL): 108 dB, Ear Coupling: Circumaural, Lightweight and Comfortable to Wear Total Harmonic Distortion Less than 0.7% With Microphone No",
+    img: 'assets/headset6.jpeg',
+    price: 1490,
+  ),
+  Item(
+      name: 'Audio Technica ATH-M20x Headphone  (Black, Over the Ear)',
+      description:
+          "Circumaural/Supraaural Circumaural Open/Closed Back Closed Back Magnet Type Neodymium Connector Plating Gold Plated Other Features Collapsible for Space-saving Portability, contours Around the Ears for Excellent Sound Isolation in Loud Environments, Designed to Excel for Studio Tracking, Mixing and Field Recording, Tuned for Enhanced Detail, with Excellent Mid-range Definition, Advanced Build Quality and Engineering, Rare Earth Magnets and Copper-clad Aluminium Wire Voice Coils, Convenient Single-side Cable Exit",
+      img: 'assets/headset7.jpeg',
+      price: 3899),
 ];
