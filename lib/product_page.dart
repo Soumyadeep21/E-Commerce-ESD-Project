93f6a4d4715bf7d00df87e790567f527edcb5347
Soumyadeep <soumo.sinha21@gmail.com>
2019-07-01 11:35:31 +0530

    Update product_page.dart

--- lib/product_page.dart
+++ lib/product_page.dart
@@ -12,47 +12,108 @@ class ProductPage extends StatelessWidget {
         title: Text('Product Details'),
         centerTitle: true,
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_shopping_cart),
-        onPressed: () {},
-      ),
-      body: Container(
-        padding: EdgeInsets.all(12.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Divider(),
-              Hero(
-                child: Image.asset(item.img),
-                tag: item.img,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 10,
+            child: Container(
+              padding: EdgeInsets.all(12.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Divider(),
+                    Center(
+                      child: Hero(
+                        child: Image.asset(
+                          item.img,
+                          height: 350,
+                          fit: BoxFit.scaleDown,
+                        ),
+                        tag: item.img,
+                      ),
+                    ),
+                    Divider(),
+                    Text(
+                      item.name,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    ),
+                    Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          '₹${item.price.toInt()}',
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                        FloatingActionButton(
+                          child: Icon(Icons.favorite_border),
+                          backgroundColor: Colors.red,
+                          mini: true,
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    Divider(),
+                    Text(
+                      'Desciption ',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Divider(),
+                    Text(
+                      item.description,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
               ),
-              Divider(),
-              Text(
-                item.name,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-              ),
-              Divider(),
-              Text(
-                '₹${item.price.toInt()}',
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              Divider(),
-              Text(
-                'Desciption ',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.w600),
-              ),
-              Divider(),
-              Text(
-                item.description,
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-              ),
-            ],
+            ),
           ),
-        ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    height: double.infinity,
+                    color: Colors.orange,
+                    child: Center(
+                      child: Text(
+                        'Buy Now',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: double.infinity,
+                    color: Colors.yellow,
+                    child: Center(
+                      child: Text(
+                        'Add to Cart',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
