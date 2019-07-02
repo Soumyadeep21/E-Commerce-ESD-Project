59c5876ae4b93b72f775d4b11e9300f9894008ed
Soumyadeep <soumo.sinha21@gmail.com>
2019-07-02 23:55:51 +0530

    Update local_stores.dart

--- lib/local_stores.dart
+++ lib/local_stores.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
 class LocalStores extends StatelessWidget {
+  final List<String> _items = [
+    'Kundu Electronics',
+    'Great Eastern',
+    'Hello Electronics',
+    'Adamas Electronics'
+  ];
+  final List<String> _address = [
+    'Shakher Bazar',
+    'Kheya Ghat',
+    'Uttarpara',
+    'Hindmotor',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Stores near you will be shown in this area !'),
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: ListView.builder(
+        itemCount: _items.length,
+        itemBuilder: (_, index) => Card(
+          elevation: 10.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Text(_items[index][0]),
+            ),
+            title: Text(_items[index]),
+            subtitle: Text(_address[index]),
+          ),
+        ),
+      ),
     );
   }
 }
